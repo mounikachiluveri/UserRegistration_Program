843fe58d23125730642d31f9f2c041a7cccbd6a7
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-23 17:46:11 +0530

    [Mounika] Added:Checking email pattern to validate the email

--- src/main/java/UserValidation.java
+++ src/main/java/UserValidation.java
@@ -1,7 +1,8 @@
 public class UserValidation {
     public static final String namePattern="^[A-Z]{1}[a-z]{2,}";
-    public static boolean userDataValidation(String dataFromTest,String namePattern){
-        return dataFromTest.matches(namePattern);
+    public static final String emailPattern = "^[A-Za-z]{3,}([.|+|_|-]?[A-Za-z0-9]+)?[@][A-Za-z0-9]+[.][A-Za-z]{2,4}([.][A-Za-z]{2,4}+)?$";
+    public static boolean userDataValidation(String dataFromTest,String Pattern){
+        return dataFromTest.matches(Pattern);
 
     }
 }
--- src/test/java/UserValidationTest.java
+++ src/test/java/UserValidationTest.java
@@ -1,10 +1,11 @@
 import org.junit.Assert;
 import org.junit.Test;
+
 public class UserValidationTest {
     @Test
     public void firstNameShouldProperReturnTrue() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("Mounika",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("Mounika", userValidation.namePattern);
         Assert.assertTrue(result);
         System.out.println(result);
     }
@@ -12,7 +13,7 @@ public class UserValidationTest {
     @Test
     public void firstNameshouldNotProperReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("Mo",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("Mo", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
 
@@ -22,7 +23,7 @@ public class UserValidationTest {
     @Test
     public void firstNameFirstLetterNotCapReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("mounika",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("mounika", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
 
@@ -31,16 +32,16 @@ public class UserValidationTest {
     @Test
     public void firstNameShouldUpperCaseReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("MOUNIKA",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("MOUNIKA", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
 
     }
 
     @Test
-    public void firstNameShouldBeDigitReturnFalse(){
+    public void firstNameShouldBeDigitReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("12345",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("12345", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
 
@@ -50,7 +51,7 @@ public class UserValidationTest {
     @Test
     public void lastNameShouldProperReturnTrue() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("Chiluveri",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("Chiluveri", userValidation.namePattern);
         Assert.assertTrue(result);
         System.out.println(result);
 
@@ -60,7 +61,7 @@ public class UserValidationTest {
     @Test
     public void lastNameShouldNotProperReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("ch",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("ch", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
     }
@@ -68,7 +69,7 @@ public class UserValidationTest {
     @Test
     public void lastNameFirstLetterShouldNotCapReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("chiluveri",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("chiluveri", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
     }
@@ -76,7 +77,7 @@ public class UserValidationTest {
     @Test
     public void lastNameShouldBeCaptitalReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("CHILUVERI",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("CHILUVERI", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
 
@@ -85,10 +86,61 @@ public class UserValidationTest {
     @Test
     public void lastNameShouldDigitReturnFalse() {
         UserValidation userValidation = new UserValidation();
-        boolean result=userValidation.userDataValidation("1999",userValidation.namePattern);
+        boolean result = userValidation.userDataValidation("1999", userValidation.namePattern);
         Assert.assertFalse(result);
         System.out.println(result);
 
     }
-}
 
+    @Test
+    public void emailShouldProperReturnTrue() {
+        UserValidation userValidation = new UserValidation();
+        boolean result = userValidation.userDataValidation("chiluveri.mounika@gmail.com", userValidation.emailPattern);
+        Assert.assertTrue(result);
+        System.out.println("Properemail");
+
+    }
+
+    @Test
+    public void emailShouldHaveSymbolReturnFalse() {
+        UserValidation userValidation = new UserValidation();
+        boolean result = userValidation.userDataValidation("abc", userValidation.emailPattern);
+        Assert.assertFalse(result);
+        System.out.println("email must contain the symbol");
+
+    }
+
+    @Test
+    public void emailNotstartWithdotReturnFalse() {
+        UserValidation userValidation = new UserValidation();
+        boolean result = userValidation.userDataValidation(".abc@gmail.com", userValidation.emailPattern);
+        Assert.assertFalse(result);
+        System.out.println("email should not start with dot");
+
+    }
+
+    @Test
+    public void emailLastAtleastTwoCharactersReturnFalse() {
+        UserValidation userValidation = new UserValidation();
+        boolean result = userValidation.userDataValidation("abc@gmail.b", userValidation.emailPattern);
+        Assert.assertFalse(result);
+        System.out.println("Email last atleast two characters");
+
+    }
+
+    @Test
+    public void emailOnlyAllowFewSymbolsReturnFalse() {
+        UserValidation userValidation = new UserValidation();
+        boolean result = userValidation.userDataValidation("abc()*@gmail",userValidation.emailPattern);
+        Assert.assertFalse(result);
+        System.out.println("email doesnot allow some symbols");
+
+    }
+    @Test
+    public void emailOnlyAllowSymbolReturnFalse() {
+        UserValidation userValidation = new UserValidation();
+        boolean result = userValidation.userDataValidation("abc@@gmail.com", userValidation.emailPattern);
+        Assert.assertFalse(result);
+        System.out.println("email doest not allow @@");
+    }
+}
